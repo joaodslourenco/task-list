3dac0e68c10abbde8c70f8a1f21aebbea0200c44
Joao Lourenco <joaodslourenco@gmail.com>
2022-01-05 09:51:06 -0300

    selection component done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,18 @@
 import Tarefa from '../model/Tarefa'
 import tarefasIniciais from '../data/mock'
+import Selecao from '../components/lista/Selecao'
 
 export default function Home() {
-  let tarefas = tarefasIniciais
-  tarefas = tarefas.filtrarAtivas()
-  tarefas = tarefas.filtrarConcluidas()
-  // tarefas = tarefas.removerFiltro()
-  tarefas = tarefas.excluirConcluidas()
-  tarefas = tarefas.adicionarTarefa(Tarefa.criarConcluida(6, 'lavar os pratos'))
-  tarefas = tarefas.adicionarTarefa(Tarefa.criarAtiva(7, 'lavar os talheres'))
-
-  tarefas = tarefas.modificarTarefa(tarefas.itens[2].alternarStatus())
-  tarefas = tarefas.modificarTarefa(tarefas.itens[1].alternarStatus())
-
-  function renderizarTarefas() {
-    return tarefas.itens.map(tarefa => {
-      return (
-        <div key={tarefa.id}>
-          <span>{tarefa.id} | </span>
-          <span>{tarefa.descricao} | </span>
-          <span>{tarefa.concluida ? 'Concluída' : 'Ativa'}</span>
-        </div>
-      )
-    })
-  }
-
   return (
     <div
       className={`
-    h-screen
-    flex flex-col bg-purple-600  justify-center items-center 
-    text-white bg-gradient-to-tr from-purple-500 to-yellow-600
+    h-screen flex flex-col justify-center items-center 
+    text-white 
+    bg-gradient-to-tr from-gray-600 to-gray-900
     `}
     >
-      {renderizarTarefas()}
+      <Selecao valor={true} />
+      <Selecao valor={false} />
     </div>
   )
 }
